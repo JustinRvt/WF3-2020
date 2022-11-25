5707a7d565bebac7c1f8b79904602966026294b6
Justin Ravat <justin.ravat@protonmail.com>
2022-11-25 16:57:48 +0100

    comment

--- Javascript/Exercices/videotheque/assets/js/vues/vue-fiche-film.js
+++ Javascript/Exercices/videotheque/assets/js/vues/vue-fiche-film.js
@@ -1,8 +1,11 @@
+// Je récupère l'identifiant unique liée au paramètre d'URL ?id=
 const monFilm = new URLSearchParams(window.location.search).get("id");
 console.log(monFilm);
 console.log(films);
 
+// Je crée une fonction qui va comparer l'ID de l'URL avec l'ID de mon objet correspondant
 const rechercheId = (film)=> {
+    // 
     return film._id == monFilm;
 }
 
